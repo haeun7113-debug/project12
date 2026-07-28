4dc381447fac22641e09f8e79633aa4943d7ad66
sondongsuk <dongseogson70@gmail.com>
2026-07-28 12:11:40 +0900

    수정

--- test.py
+++ test.py
@@ -2,8 +2,4 @@ print("테스트")
 print("faker is god, you must be worship him")
 print("test")
 1234
-<<<<<<< HEAD
->>>>>>> 5c5eb83b60f0aee86e5aad4e6521529e32100711
-=======
-5678
->>>>>>> 977a0498eb3ef746c9feb3ed97e75a52355061f5
+
